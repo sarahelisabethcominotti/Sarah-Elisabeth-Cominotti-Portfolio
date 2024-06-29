ae6a1ff7a6eee8383e0b61ff25b4e178d104149c
sarahelisabethcominotti <138233386+sarahelisabethcominotti@users.noreply.github.com>
2024-06-29 11:00:11 +0100

    Getting data for card from GraphQL

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,10 +1,38 @@
 import './App.css'
+import { useQuery } from "@tanstack/react-query";
+import {CARDS_API, getAllCards} from './CARDS_API'
 
 function App() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["cards"],
+    queryFn: async () => {
+      const res = await fetch(CARDS_API, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+         },
+        body: JSON.stringify({
+          query: getAllCards
+        }),
+      });
+      if (!res.ok) throw new Error("Failed to fetch");
+      return await res.json();
+    },
+  });
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+console.log(data)
 
   return (
     <>
       <h1>Vite + React</h1>
+      <div>
+      <h1>Cars</h1>
+      {JSON.stringify(data)}
+    </div>
       <div className="card">
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
---
+++ Portfolio/src/CARDS_API.js
@@ -0,0 +1,21 @@
+
+export const CARDS_API =
+  "https://api-eu-west-2.hygraph.com/v2/clxx583h704wj08w4kt0whrt9/master";
+
+export const getAllCards = `
+query MyQuery {
+  portfolioCards {
+    id
+    description
+    projectLink
+    tag
+    title
+    backgroundImage {
+      url
+    }
+  }
+}
+`;
+
+
+export default { CARDS_API, getAllCards }
