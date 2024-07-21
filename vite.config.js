72aa1b2a7280ac1eaf420c7eb606ca6b88fd9983
sarahelisabethcominotti <138233386+sarahelisabethcominotti@users.noreply.github.com>
2024-07-21 10:09:51 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true, // Automatically open the browser on server start
-    port: 3000, // Ensure the port is not conflicting with another service
-  },
 });
 
