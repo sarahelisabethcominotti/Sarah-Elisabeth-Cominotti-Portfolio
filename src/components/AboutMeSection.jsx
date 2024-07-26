db75245e1c3d447aac81e2553066c10cf8365b10
sarahelisabethcominotti <138233386+sarahelisabethcominotti@users.noreply.github.com>
2024-07-26 16:06:02 +0100

    Update of image from link

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -6,7 +6,7 @@ function HeaderSection() {
 
       <div className="about-section">
       <div>
-        <img src="dist/assets/sarah-elisabeth-cominotti-profile-picture.png" alt="picture of Sarah Elisabeth Cominotti"></img>
+        <img src="https://images2.imgbox.com/a5/a1/tXEFc44w_o.png" alt="picture of Sarah Elisabeth Cominotti"></img>
      
           <p>
             I&apos;m a <span className="highlight-word">Junior Developer</span>{" "}
